320b9b67501e3d00032a108383c4e6b6d7f89b2e
Ronalmoo <ydaniel0826@gmail.com>
2019-09-24 00:53:42 +0900

    # Trying to fix rnn_model(on_going)

--- nmt/model/ops.py
+++ nmt/model/ops.py
@@ -1,94 +1,62 @@
+import math
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-
-from torchtext.datasets import TranslationDataset, Multi30k
-from torchtext.data import Field, BucketIterator
-
-import spacy
-
-import random
-import math
-import time
+import torch.functional as F
 
 
-class Encoder(nn.Module):
-    def __init__(self, input_dim, emb_dim, enc_hid_dim, dec_hid_dim, dropout):
-        super().__init__()
+class RNNModel(nn.Module):
+    def __init__(self, rnn_type, n_token, input_size, hidden_size, n_layers, dropout=0.5,
+                 tie_weights=False):
+        super(RNNModel, self).__init__()
+        self.drop = nn.Dropout(dropout)
+        self.encoder = nn.Embedding(n_token, input_size)
+        if rnn_type in ['LSTM', 'GRU']:
+            # 속성 확인 check attribute
+            self.rnn = getattr(nn, rnn_type)(input_size, hidden_size, n_layers, dropout=dropout)
+        else:
+            try:
+                nonlinearity = {'RNN_TANH': 'tanh', 'RNN_RELU': 'relu'}[rnn_type]
+            except KeyError:
+                raise ValueError("""An invalid option for `--model` was supplied,
+                                             options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""")
+            self.rnn = nn.RNN(input_size, hidden_size, n_layers, nonlinearity=nonlinearity, dropout=dropout)
+        self.decoder = nn.Linear(hidden_size, n_token)
 
-        self.input_dim = input_dim
-        self.emb_dim = emb_dim
-        self.enc_hid_dim = enc_hid_dim
-        self.dec_hid_dim dec_hid_dim
-        self.dropout = dropout
+        if tie_weights:
+            if hidden_size != input_size:
+                raise ValueError('When using the tied flag, nhid must be equal to emsize')
+            self.decoder.weight = self.encoder.weight
 
-        self.embedding = nn.Embedding(input_dim, emb_dim)
-        self.rnn = nn.GRU(emb_dim, enc_hid_dim, dec_hid_dim, bidirectional=True)
-        self.fc = nn.Linear(enc_hid_dim * 2, dec_hid_dim)
-        self.activation = nn.Tanh()
-        self.dropout = nn.Dropout(dropout)
+        self.init_weights()
 
-    def forward(self, src):
-        # |src| = [src sent len, batch_size]
-        embedded = self.dropout(self.embedding(src))
+        self.rnn_type = rnn_type
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
 
-        # embedded = [src_sent_len, batch_size, emb_dim]
-        outputs, hidden = self.rnn(embedded)
+    def init_weights(self):
+        init_range = 0.1
+        self.encoder.weight.data.uniform_(-init_range, init_range)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-init_range, init_range)
 
-        # outputs = [src_sent_len, batch_size, hid_dim * num_directions]
-        # hidden = [n_layers * num_directions, batch_size, hid_dim]
+    def forward(self, input, hidden):
+        emb = self.drop(self.encoder(input))
+        output, hidden = self.rnn(emb, hidden)
+        output = self.drop(output)
+        decoded = self.decoder(output)
+        return decoded, hidden
 
-        # hidden is stacked [forward_1, backward_1, forward_2, backward_2, ...]
-        # outputs are always from the last layer
-
-        # hidden[-2, :, : ] is the last of the forwards RNN
-        # hidden[-1, :, : ] is the last of the backwards RNN
-
-        # initial decoder hidden is final hidden state of the forwards and backwards
-        # encoder RNNs fed through a linear layer
-        hidden = torch.tanh(self.fc(torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1)))
-
-        # outputs = [src_sent_len, batch_size, enc_hid_dim * 2]
-        # hidden = [batch_size, dec_hid_dim]
-
-        return outputs, hidden
+    def init_hidden(self, batch_size):
+        weight = next(self.parameters())
+        if self.rnn_type == 'LSTM':
+            return (weight.new_zeros(self.n_layers, batch_size, self.hidden_size),
+                    weight.new_zeros(self.n_layers, batch_size, self.hidden_size))
+        else:
+            return weight.new_zeros(self.n_layers, batch_size, self.hidden_size)
 
 
-class Attention(nn.Module):
-    def __init__(self, enc_hid_dim, dec_hid_dim):
-        super().__init__()
 
-        self.enc_hid_dim = enc_hid_dim
-        self.dec_hid_dim = dec_hid_dim
 
-        self.attn = nn.Linear((enc_hid_dim * 2) + dec_hid_dim, dec_hid_dim)
-        self.v = nn.Paramer(torch.rand(dec_hid_dim))
-
-    def forward(self, hidden, encoder_outputs):
-        # hidden = [batch_size, dec_hid_dim]
-        # encoder_outputs = [src_sent_len, batch_size, enc_hid_dim * 2]
-        batch_size = encoder_outputs.shape[1]
-        src_len = encoder_outputs.shape[0]
-
-        # repeat encoder hidden state src_len times
-        hidden = hidden.unsqueeze(1).repear(1, src_len, 1)
-        encoder_outputs = encoder_outputs.permute(1, 0, 2)
-
-        # hidden = [batch_size, src_sent_len, dec_hid_dim]
-        # encoder_outputs = [batch_size, src_sent_len, enc_hid_dim * 2]
-
-        energy = torch.tanh(self.attn(torch.cat((hidden, encoder_outputs), dim=2)))
-
-        # energy = [batch_size, src_sent_len, dec_hid_dim]
-        energy = energy.permute(0, 2, 1)
-        # energy = [batch_size, dec_hid_dim, src_sent_len]
-        # v = [dec_hid_dim]
-        v = self.v.repeat(batch_size, 1).unsqueeze(1)
-        # v = [batch_size, 1, dec_hid_dim]
-        attention = torch.bmm(v, energy).squeeze(1)
-        # attention = [batch_size, src_len]
-        return F.softmax(attention, dim=1)
 
 
 
