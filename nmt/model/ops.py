a551f97dce169d03acdade88d8edc39d03fb9f45
Ronalmoo <ydaniel0826@gmail.com>
2019-09-24 16:14:00 +0900

    # positional encoding

--- nmt/model/ops.py
+++ nmt/model/ops.py
@@ -55,7 +55,50 @@ class RNNModel(nn.Module):
             return weight.new_zeros(self.n_layers, batch_size, self.hidden_size)
 
 
+class PositionalEncoding(nn.Module):
+    """
+    트랜스포머는 RNN을 이용하지 않으므로, 순서 정보를 단어와 함께 주어야한다.
+    같은 단어라 하더라도 위치에 따라 그 쓰임새와 역할의 의미가 달라질 수 있기 때문
+    Args:
+        emb_dim: the embed dim(required)
+        dropout: the dropout value(default=0.1
+        max_len: the max. length of the incoming sequence(default=5000)
+    """
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = dropout
 
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
+
+class TransformerModel(nn.Module):
+    def __init__(self, n_token, input_size, n_head, hidden_size, n_layers, dropout=0.5):
+        super(TransformerModel, self).__init__()
+        try:
+            from torch.nn import TransformerEncoder, TransformerEncoderLayer
+        except:
+            raise ImportError('TransformerEncoder module does not exist in PyTorch 1.1 or lower.')
+
+        self.model_type = 'Transformer'
+        self.src_mask = None
+        self.pos_encoder = PositionalEncoding(input_size, dropout)
+        encoder_layers = TransformerEncoderLayer(input_size, n_head, hidden_size, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, n_layers)
+        self.encoder = nn.Embedding(n_token, input_size)
+        self.input_size = input_size
+        self.decoder = nn.Linear(input_size, n_token)
+
+        self.init_weigths()
 
 
 
